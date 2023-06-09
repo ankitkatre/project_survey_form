bdeca3daf708f71082b29376dba7c3c19035f2e8
Ankit Katre <89594184+ankitkatre@users.noreply.github.com>
2023-06-09 16:50:38 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,117 +1,182 @@
-const DATABASE_KEY = 'customerSurvey';
 const questions = [
-  { id: 1, text: 'How satisfied are you with our products?', type: 'rating' },
-  { id: 2, text: 'How fair are the prices compared to similar retailers?', type: 'rating' },
-  { id: 3, text: 'How satisfied are you with the value for money of your purchase?', type: 'rating' },
-  { id: 4, text: 'On a scale of 1-10, how likely are you to recommend us to your friends and family?', type: 'recommendation' },
-  { id: 5, text: 'What could we do to improve our service?', type: 'text' }
+  {
+    id: 1,
+    question: 'How satisfied are you with our products?',
+    type: 'rating',
+    options: [1, 2, 3, 4, 5]
+  },
+  {
+    id: 2,
+    question: 'How fair are the prices compared to similar retailers?',
+    type: 'rating',
+    options: [1, 2, 3, 4, 5]
+  },
+  {
+    id: 3,
+    question: 'How satisfied are you with the value for money of your purchase?',
+    type: 'rating',
+    options: [1, 2, 3, 4, 5]
+  },
+  {
+    id: 4,
+    question: 'On a scale of 1-10, how likely are you to recommend us to your friends and family?',
+    type: 'rating',
+    options: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  },
+  {
+    id: 5,
+    question: 'What could we do to improve our service?',
+    type: 'text'
+  }
 ];
 
 let currentQuestionIndex = 0;
-let customerSessionId = generateSessionId();
-let answers = {};
 
-function startSurvey() {
-  document.getElementById('welcome-screen').style.display = 'none';
-  document.getElementById('survey-screen').style.display = 'block';
-  showQuestion();
+function initializeSurvey() {
+  const startBtn = document.getElementById('startBtn');
+  const surveyContainer = document.getElementById('surveyContainer');
+
+  startBtn.addEventListener('click', () => {
+    startBtn.style.display = 'none';
+    surveyContainer.style.display = 'block';
+    showQuestion(currentQuestionIndex);
+  });
 }
 
-function showQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  document.getElementById('question-counter').textContent = `Question ${currentQuestionIndex + 1}/${questions.length}`;
-  document.getElementById('question-text').textContent = currentQuestion.text;
+function showQuestion(questionIndex) {
+  const questionNumber = document.getElementById('questionNumber');
+  const questionElement = document.getElementById('question');
+  const answerContainer = document.getElementById('answerContainer');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
 
-  document.getElementById('rating-container').style.display = 'none';
-  document.getElementById('recommendation-container').style.display = 'none';
-  document.getElementById('text-answer-container').style.display = 'none';
-  document.getElementById('previous-btn').disabled = currentQuestionIndex === 0;
-  document.getElementById('next-btn').textContent = currentQuestionIndex === questions.length - 1 ? 'Finish' : 'Next';
-  if (currentQuestion.type === 'rating') {
-    document.getElementById('rating-container').style.display = 'block';
-  } else if (currentQuestion.type === 'recommendation') {
-    document.getElementById('recommendation-container').style.display = 'block';
-  } else if (currentQuestion.type === 'text') {
-    document.getElementById('text-answer-container').style.display = 'block';
+  const submitBtn = document.getElementById('submitBtn');
+
+  const { id, question, type, options } = questions[questionIndex];
+
+  questionNumber.textContent = `Question ${questionIndex + 1}/${questions.length}`;
+  questionElement.textContent = question;
+  answerContainer.innerHTML = '';
+
+  if (type === 'rating') {
+    const radioGroup = document.createElement('div');
+    radioGroup.className = 'radio-group';
+
+    options.forEach(option => {
+      const radioBtn = document.createElement('input');
+      radioBtn.type = 'radio';
+      radioBtn.name = 'answer';
+      radioBtn.value = option;
+      radioGroup.appendChild(radioBtn);
+
+      const label = document.createElement('label');
+      label.textContent = option;
+      radioGroup.appendChild(label);
+    });
+
+    answerContainer.appendChild(radioGroup);
+  } else if (type === 'text') {
+    const textArea = document.createElement('textarea');
+    textArea.name = 'answer';
+    answerContainer.appendChild(textArea);
   }
-}
-function previousQuestion() {
-  currentQuestionIndex--;
-  showQuestion();
-}
 
-function nextQuestion() {
-  const currentQuestion = questions[currentQuestionIndex];
-  let answer = null;
+  prevBtn.disabled = questionIndex === 0;
 
-  if (currentQuestion.type === 'rating') {
-    const ratingInputs = document.getElementsByName('rating');
-    for (const input of ratingInputs) {
-      if (input.checked) {
-        answer = parseInt(input.value);
-        break;
-      }
-    }
-  } else if (currentQuestion.type === 'recommendation') {
-    answer = parseInt(document.getElementById('recommendation-rating').value);
-  } else if (currentQuestion.type === 'text') {
-    answer = document.getElementById('text-answer').value;
-  }
+  if (questionIndex === questions.length - 1) {
+    nextBtn.style.display = 'none';
 
-  answers[currentQuestion.id] = answer;
-  currentQuestionIndex++;
-
-  if (currentQuestionIndex === questions.length) {
-    saveSurveyData();
-    showThankYouScreen();
+    submitBtn.style.display = 'block';
   } else {
-    showQuestion();
+    nextBtn.style.display = 'block';
+
+    submitBtn.style.display = 'none';
+  }
+
+  bindAnswerEvents();
+}
+
+function bindAnswerEvents() {
+  const answerInputs = document.querySelectorAll('input[name="answer"]');
+  const textArea = document.querySelector('textarea[name="answer"]');
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  const submitBtn = document.getElementById('submitBtn');
+
+  answerInputs.forEach(input => {
+    input.addEventListener('change', () => {
+      nextBtn.disabled = false;
+    });
+  });
+
+  if (textArea) {
+    textArea.addEventListener('input', () => {
+      nextBtn.disabled = false;
+    });
+  }
+
+  prevBtn.addEventListener('click', () => {
+    currentQuestionIndex--;
+    showQuestion(currentQuestionIndex);
+  });
+
+  nextBtn.addEventListener('click', () => {
+    saveAnswer();
+    currentQuestionIndex++;
+    showQuestion(currentQuestionIndex);
+  });
+
+
+
+  // submitBtn.addEventListener('click', () => {
+    // if (confirm('Are you sure you want to submit the survey?')) {
+      // saveAnswer('');
+      // setSurveyStatus('COMPLETED');
+      // showThankYouScreen();
+    // }
+  // });
+}
+
+function saveAnswer() {
+  const answerInputs = document.querySelectorAll('input[name="answer"]');
+  const textArea = document.querySelector('textarea[name="answer"]');
+
+  let answer;
+
+  if (answerInputs.length > 0) {
+    answer = Array.from(answerInputs).find(input => input.checked)?.value;
+  } else if (textArea) {
+    answer = textArea.value;
+  }
+
+  if (answer) {
+    const questionId = questions[currentQuestionIndex].id;
+    localStorage.setItem(`answer_${questionId}`, answer);
   }
 }
 
-function skipQuestion() {
-  currentQuestionIndex++;
-  showQuestion();
-}
-
-function saveSurveyData() {
-  let surveyData = JSON.parse(localStorage.getItem(DATABASE_KEY)) || {};
-  surveyData[customerSessionId] = answers;
-  localStorage.setItem(DATABASE_KEY, JSON.stringify(surveyData));
+function setSurveyStatus(status) {
+  localStorage.setItem('surveyStatus', status);
 }
 
 function showThankYouScreen() {
-  document.getElementById('survey-screen').style.display = 'none';
-  document.body.innerHTML = '<h1>Thank you for taking the survey!</h1>';
+  const surveyContainer = document.getElementById('surveyContainer');
+  const thankYouContainer = document.getElementById('thankYouContainer');
+
+  surveyContainer.style.display = 'none';
+  thankYouContainer.style.display = 'block';
+
+  setTimeout(() => {
+    surveyContainer.style.display = 'block';
+    thankYouContainer.style.display = 'none';
+    resetSurvey();
+  }, 5000);
 }
 
-function generateSessionId() {
-  // Generate a random session ID
-  const timestamp = new Date().getTime();
-  const randomId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  return `${timestamp}-${randomId}`;
+function resetSurvey() {
+  currentQuestionIndex = 0;
+  const startBtn = document.getElementById('startBtn');
+  startBtn.style.display = 'block';
 }
-function showThankYouScreen() {
-    document.getElementById('survey-screen').style.display = 'none';
-  
-    // Show confirmation dialog
-    const confirmed = confirm('Are you sure you want to submit the survey?');
-    if (confirmed) {
-      saveSurveyData();
-      setSurveyStatus('COMPLETED');
-      document.body.innerHTML = '<h1>Thank you for taking the survey!</h1>';
-      setTimeout(showWelcomeScreen, 5000);
-    } else {
-      showWelcomeScreen();
-    }
-  }
-  
-  function showWelcomeScreen() {
-    document.body.innerHTML = `
-      <div id="welcome-screen">
-        <h1>Welcome back!</h1>
-        <button onclick="startSurvey()">Start</button>
-      </div>
-    `;
-  }
-  
+
+window.addEventListener('DOMContentLoaded', initializeSurvey);
